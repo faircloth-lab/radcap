155a61040e1d6f3632d73aad8212d83ecca4073f
Brant Faircloth <brant@faircloth-lab.org>
2015-11-10 15:14:15 -0600

    added flagstat summary

---
+++ bin/radcap_get_flagstats.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+(c) 2015 Brant Faircloth || http://faircloth-lab.org/
+All rights reserved.
+
+This code is distributed under a 3-clause BSD license. Please see
+LICENSE.txt for more information.
+
+Created on 10 November 2015 14:32 CST (-0600)
+"""
+
+import os
+import argparse
+
+from radcap import samtools
+from radcap.log import setup_logging
+from radcap.helpers import FullPaths, is_dir
+
+
+import pdb
+
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+        description="""Given an input directory containing bam files, output samtools flagstat info for all BAM files"""
+    )
+    parser.add_argument(
+        "--input-dir",
+        required=True,
+        action=FullPaths,
+        default=None,
+        help="""The input directory containing BAM files"""
+    )
+    parser.add_argument(
+        "--flag",
+        action="store_true",
+        default=False,
+        help="""Help text""",
+    )
+    parser.add_argument(
+        "--verbosity",
+        type=str,
+        choices=["INFO", "WARN", "CRITICAL"],
+        default="INFO",
+        help="""The logging level to use"""
+    )
+    parser.add_argument(
+        "--log-path",
+        action=FullPaths,
+        type=is_dir,
+        default=None,
+        help="""The path to a directory to hold logs."""
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    # setup logging
+    log, my_name = setup_logging(args)
+    all_bams = []
+    for root, dirs, files in os.walk(args.input_dir, followlinks=False):
+        bams = [f for f in files if ".bam" in f]
+        if bams != []:
+            for b in bams:
+                all_bams.append(os.path.join(root, b))
+        else:
+            pass
+    # pretty print taxon status
+    text = " samtools flagstat output"
+    log.info(text.center(65, "-"))
+    log.info("bam, reads, duplicates, mapped, percent mapped, mate mapped, percent mate mapped")
+    for bam in all_bams:
+        samtools.flagstats(log, bam)
+    # end
+    text = " Completed {} ".format(my_name)
+    log.info(text.center(65, "="))
+
+
+if __name__ == '__main__':
+    main()
---
+++ radcap/samtools.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+(c) 2015 Brant Faircloth || http://faircloth-lab.org/
+All rights reserved.
+
+This code is distributed under a 3-clause BSD license. Please see
+LICENSE.txt for more information.
+
+Created on 10 November 2015 14:25 CST (-0600)
+"""
+
+
+import os
+import re
+import subprocess
+
+from radcap.pth import get_user_param, get_user_path
+
+import pdb
+
+
+def flagstats(log, bam):
+    cmd = [get_user_path("samtools", "samtools"), "flagstat", bam]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    result = re.search("(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n(\d+)\s+.*\n", stdout)
+    result = [int(i) for i in result.groups()]
+    names = [
+        "total",
+        "duplicates",
+        "mapped",
+        "paired",
+        "read1",
+        "read2",
+        "properly_paired",
+        "mate_mapped",
+        "singletons",
+        "diff_chromo",
+        "hq_diff_chromo"
+    ]
+    result = dict(zip(names, result))
+    log.info("{}, {}, {}, {}, {}, {},{}".format(
+        os.path.basename(bam),
+        result['total'],
+        result['duplicates'],
+        result['mapped'],
+        round(float(result['mapped'])/result['total'] * 100, 2),
+        result['mate_mapped'],
+        round(float(result['mate_mapped'])/result['mapped'] * 100, 2)
+    ))
+
