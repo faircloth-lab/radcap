cf9b25b78bc147ce3f738f13f7a13d95ec2c49eb
Brant Faircloth <brant@faircloth-lab.org>
2015-12-11 14:59:42 -0600

    fix variable and pad string

--- bin/radcap_summarize_snp_calls.py
+++ bin/radcap_summarize_snp_calls.py
@@ -75,12 +75,12 @@ def main():
     ordered_status = cnt_status.keys()
     ordered_status.sort()
     for status in ordered_status:
-        log.info("{}\tSNPs with {}".format(cnt_status[status], status))
+        log.info("{0: <10}\tSNPs with {}".format(cnt_status[status], status))
     log.info("Mean SNPs per locus\t = {}".format(numpy.mean(snps_per_locus)))
     log.info("95 CI SNPs per locus\t = {}".format(1.96 * (numpy.std(snps_per_locus, ddof=1) / numpy.sqrt(len(snps_per_locus)))))
     log.info("Max SNPs per locus\t = {}".format(numpy.min(snps_per_locus)))
     log.info("Min SNPs per locus\t = {}".format(numpy.max(snps_per_locus)))
-    if args.output is not None:
+    if args.output_file is not None:
         with open(args.output_file, "w") as outfile:
             outfile.write("locus,count\n")
             for locus, cnt in cnt_loci.iteritems():
