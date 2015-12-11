51bdcfe4b42d8b2e2272c47f81fe9227e7d38c07
Brant Faircloth <brant@faircloth-lab.org>
2015-12-11 14:53:24 -0600

    reformat log output and create outfile

--- bin/radcap_summarize_snp_calls.py
+++ bin/radcap_summarize_snp_calls.py
@@ -30,7 +30,14 @@ def get_args():
         required=True,
         action=FullPaths,
         default=None,
-        help="""The input directory containing BAM files"""
+        help="""The input VCF file to summarize"""
+    )
+    parser.add_argument(
+        "--output-file",
+        required=False,
+        action=FullPaths,
+        default=None,
+        help="""The output file to hold summary counts per locus"""
     )
     parser.add_argument(
         "--verbosity",
@@ -61,17 +68,23 @@ def main():
                 pass
             else:
                 ls = line.strip().split("\t")
-                cnt_loci[ls[0]] += 1
                 cnt_status[ls[6]] += 1
+                if ls[6] == "PASS":
+                    cnt_loci[ls[0]] += 1
     snps_per_locus = numpy.array([v[1] for v in cnt_loci.iteritems()])
     ordered_status = cnt_status.keys()
     ordered_status.sort()
     for status in ordered_status:
-        log.info("SNPs with {}\t\t= {}".format(cnt_status[status]))
-    log.info("Mean SNPs per locus\t\t = {}".format(numpy.mean(snps_per_locus)))
-    log.info("95 CI SNPs per locus\t\t = {}".format(1.96 * (numpy.std(snps_per_locus, ddof=1) / numpy.sqrt(len(snps_per_locus)))))
-    log.info("Max SNPs per locus\t\t = {}".format(numpy.min(snps_per_locus)))
-    log.info("Min SNPs per locus\t\t = {}".format(numpy.max(snps_per_locus)))
+        log.info("{}\tSNPs with {}".format(cnt_status[status], status))
+    log.info("Mean SNPs per locus\t = {}".format(numpy.mean(snps_per_locus)))
+    log.info("95 CI SNPs per locus\t = {}".format(1.96 * (numpy.std(snps_per_locus, ddof=1) / numpy.sqrt(len(snps_per_locus)))))
+    log.info("Max SNPs per locus\t = {}".format(numpy.min(snps_per_locus)))
+    log.info("Min SNPs per locus\t = {}".format(numpy.max(snps_per_locus)))
+    if args.output is not None:
+        with open(args.output, "w") as outfile:
+            outfile.write("locus,count\n")
+            for locus, cnt in cnt_loci.iteritems():
+                outfile.write("{},{}\n".format(locus, cnt))
     # end
     text = " Completed {} ".format(my_name)
     log.info(text.center(65, "="))
